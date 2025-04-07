bf843967621876df87962b18395834f4848d8fb3
Ian Zhang <iazhang@cisco.com>
2025-04-07 16:10:50 +0800

    Avoid using walrus operator which is not supported in legacy Python version

--- pnp_utils.py
+++ pnp_utils.py
@@ -101,8 +101,10 @@ def calculate_md5(filepath):
     try:
         with open(filepath, mode='rb') as file:
             md5_hash = hashlib.md5()
-            while chunk := file.read(8192):
+            chunk = file.read(8192)
+            while chunk:
                 md5_hash.update(chunk)
+                chunk = file.read(8192)
         return md5_hash.hexdigest()
     except FileNotFoundError:
         print(f"The file '{filepath}' does not exist!")
